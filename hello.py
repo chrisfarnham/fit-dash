f9409818a6afd3c3afe1afadf7627451de97907f
Chris Farnham <chris.farnham@gmail.com>
2016-06-02 01:34:33 +0000

    added connect stuff

--- hello.py
+++ hello.py
@@ -1,12 +1,36 @@
 from flask import Flask
+from flask.ext.mysqldb import MySQL
 import os 
 
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DB')
+
+mysql.init_app(app)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	cursor = mysql.connect().cursor()
+	# test sql statement
+	sql = 'SELECT * FROM authors LIMIT 1'
+	cursor.execute(sql)
+	data=cursor.fetchall()
+	
+	for f in data :
+		theID = f[0]
+		theName = f[1]
+		theEmail = f[2]
+	
+	html = str(theID) + ", " + theName + ", " + theEmail
+	html += html + '<br> Hello, World!'
+	
+	return html
     
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
